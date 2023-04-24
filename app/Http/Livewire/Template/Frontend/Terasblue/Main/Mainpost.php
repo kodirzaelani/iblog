da378c806705113841127d7af1d042e4c5f99044
kodirzaelani <laman.kreasi@gmail.com>
2023-04-24 09:43:23 +0800

    add mainpost

---
+++ app/Http/Livewire/Template/Frontend/Terasblue/Main/Mainpost.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Template\Frontend\Terasblue\Main;
+
+use Livewire\Component;
+
+class Mainpost extends Component
+{
+    public function render()
+    {
+        return view('livewire.template.frontend.terasblue.main.mainpost');
+    }
+}
---
+++ resources/views/livewire/template/frontend/terasblue/main/mainpost.blade.php
@@ -0,0 +1,183 @@
+<div>
+    <div class="row">
+
+        <!-- berita section -->
+        <div class="col-md-12 mb-3">
+            <h4><i class="bi bi-book-half"></i> BERITA TERBARU</h4>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+                <div class="card-footer bg-white">
+                    <i class="fa fa-calendar" aria-hidden="true"></i> 09 Juli 2020
+                </div>
+            </div>
+        </div>
+        <!-- end berita section -->
+
+        <!-- foto section -->
+        <div class="col-md-12 mb-3 mt-4">
+            <h4><i class="fas fa-images"></i> FOTO TERBARU</h4>
+        </div>
+
+
+        <div class="col-md-6 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- send foto section -->
+
+        <!-- video section -->
+        <div class="col-md-12 mb-3 mt-4">
+            <h4><i class="fas fa-video"></i> VIDEO TERBARU</h4>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <div class="card h-100 shadow-sm border-0 rounded-lg">
+                <div class="card-img">
+                    <img src="{{ asset('') }}assets/frontend/images/example/post_image.png" class="w-100"
+                        style="height: 200px;object-fit: cover;border-top-left-radius: .3rem;border-top-right-radius: .3rem;">
+                </div>
+                <div class="card-body">
+                    <a href="http://" class="text-dark text-decoration-none">
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- end video section -->
+
+    </div>
+</div>
